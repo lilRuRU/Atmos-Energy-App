99484de8395bb2619882f362d9f8778f800334c7
KingRuRu <>
2018-07-10 10:51:15 -0500

    Customer Info

---
+++ customerInformationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  customerInformationViewController.swift
+//  App
+//
+//  Created by mscoding on 7/10/18.
+//  Copyright © 2018 mscoding. All rights reserved.
+//
+
+import UIKit
+
+class customerInformationViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - 
+}
---
+++ searchCustomerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  searchCustomerViewController.swift
+//  App
+//
+//  Created by mscoding on 7/10/18.
+//  Copyright © 2018 mscoding. All rights reserved.
+//
+
+import UIKit
+
+class searchCustomerViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+  
+
+}
